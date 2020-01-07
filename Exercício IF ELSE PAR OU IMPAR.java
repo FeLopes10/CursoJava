97dbf774e4f15054cddaefde967c257ff842a33c
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2020-01-07 09:55:54 -0300

    Create Exercício IF ELSE PAR OU IMPAR.java

---
+++ Exercício IF ELSE PAR OU IMPAR.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N;
+
+		System.out.println("Par ou Impar?");
+		N = sc.nextInt();
+
+		if (N % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+				System.out.println("IMPAR");
+		} 
+		
+	}
+}
