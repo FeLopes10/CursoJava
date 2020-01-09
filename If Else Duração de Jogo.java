2e5ee8c516e59b23b6feb2d06aa3992950abd2aa
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2020-01-09 15:10:45 -0300

    Update If Else Duração de Jogo.java

--- If Else Duração de Jogo.java
+++ If Else Duração de Jogo.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+	
+/* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
 
 	public static void main(String[] args) {
 
