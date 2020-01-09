cda2f34a1e807d1d2855b2029096345338676637
FeLopes10 <58078076+FeLopes10@users.noreply.github.com>
2020-01-09 15:41:39 -0300

    Update If Else PAR ou IMPAR.java

--- If Else PAR ou IMPAR.java
+++ If Else PAR ou IMPAR.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 
+/* Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. */
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
